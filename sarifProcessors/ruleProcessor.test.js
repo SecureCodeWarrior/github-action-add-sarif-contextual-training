bc5a3d2d6dea05bc418eb76d259fd4db401dc991
Colin Wong <cwong@securecodewarrior.com>
2020-09-24 14:18:25 +1000

    Added scanning of rule id and name

--- sarifProcessors/ruleProcessor.test.js
+++ sarifProcessors/ruleProcessor.test.js
@@ -86,3 +86,39 @@ test('ruleProcessor should load test002 and add contextual micro-learning materi
         }
     });
 });
+
+test('ruleProcessor should load test003 and add 2 entries based on the rule id and name', async () => {
+    const sarif = await sarifLoader.load('./fixtures/test003.sarif');
+    const name = 'AAA';
+    const description = 'bbb';
+    const url = 'ccc';
+    const videos = ['ddd'];
+    directLinking.getTrainingData.mockResolvedValue({
+        name,
+        description,
+        url,
+        videos
+    });
+    await ruleProcessor.process(sarif.runs[0]);
+
+    // expect material added to help.text and help.markdown
+    expect(sarif.runs[0].tool.driver.rules[0]).toEqual({
+        "id": "TEST01 CWE-22",
+        "name": "Test 01 rule name cwe: 23",
+        "messageStrings": {
+          "default": {
+            "text": "This is the message text. It might be very long."
+          }
+        },
+        "shortDescription": {
+          "text": "Failed to release dynamic memory."
+        },
+        "fullDescription": {
+          "text": "Unused variables, imports, functions or classes may be a symptom of a bug and should be examined carefully."
+        },
+        "help": {
+          "text": `some help text\n\nSecure Code Warrior Training:\n\n${name} - ${description} [Train Now](${url}) or [watch an explainer video](${videos[0]})\n\n${name} - ${description} [Train Now](${url}) or [watch an explainer video](${videos[0]})`,
+          "markdown": `markdown version some link [here](https://github.com)\n\n## Secure Code Warrior Training\n\n#### ${name}\n\n${description}\n\n**[Train Now](${url})** or [watch an explainer video](${videos[0]})\n\n#### ${name}\n\n${description}\n\n**[Train Now](${url})** or [watch an explainer video](${videos[0]})`
+        }
+      });
+});
