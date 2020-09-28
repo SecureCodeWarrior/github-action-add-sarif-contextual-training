4d4b31fad7d44bd205fb5b54a780da1be9aefabc
Colin Wong <cwong@securecodewarrior.com>
2020-09-28 10:17:06 +1000

    Added support for rule tags

--- sarifProcessors/ruleProcessor.test.js
+++ sarifProcessors/ruleProcessor.test.js
@@ -107,19 +107,63 @@ test('ruleProcessor should load test003 and add 2 entries based on the rule id a
         "id": "TEST01 CWE-22",
         "name": "Test 01 rule name cwe: 23",
         "messageStrings": {
-          "default": {
-            "text": "This is the message text. It might be very long."
-          }
+            "default": {
+                "text": "This is the message text. It might be very long."
+            }
         },
         "shortDescription": {
-          "text": "Failed to release dynamic memory."
+            "text": "Failed to release dynamic memory."
         },
         "fullDescription": {
-          "text": "Unused variables, imports, functions or classes may be a symptom of a bug and should be examined carefully."
+            "text": "Unused variables, imports, functions or classes may be a symptom of a bug and should be examined carefully."
         },
         "help": {
-          "text": `some help text\n\nSecure Code Warrior Training:\n\n[CWE 22] ${NAME} [What is this?](${VIDEOS[0]})\n\n${DESCRIPTION} [Try this challenge in Secure Code Warrior](${URL})\n\n[CWE 23] ${NAME} [What is this?](${VIDEOS[0]})\n\n${DESCRIPTION} [Try this challenge in Secure Code Warrior](${URL})`,
-          "markdown": `markdown version some link [here](https://github.com)\n\n## Secure Code Warrior Training\n\n#### [CWE 22] ${NAME} *[What is this?](${VIDEOS[0]})*\n\n* ${DESCRIPTION} [Try this challenge in Secure Code Warrior](${URL})\n\n#### [CWE 23] ${NAME} *[What is this?](${VIDEOS[0]})*\n\n* ${DESCRIPTION} [Try this challenge in Secure Code Warrior](${URL})`
+            "text": `some help text\n\nSecure Code Warrior Training:\n\n[CWE 22] ${NAME} [What is this?](${VIDEOS[0]})\n\n${DESCRIPTION} [Try this challenge in Secure Code Warrior](${URL})\n\n[CWE 23] ${NAME} [What is this?](${VIDEOS[0]})\n\n${DESCRIPTION} [Try this challenge in Secure Code Warrior](${URL})`,
+            "markdown": `markdown version some link [here](https://github.com)\n\n## Secure Code Warrior Training\n\n#### [CWE 22] ${NAME} *[What is this?](${VIDEOS[0]})*\n\n* ${DESCRIPTION} [Try this challenge in Secure Code Warrior](${URL})\n\n#### [CWE 23] ${NAME} *[What is this?](${VIDEOS[0]})*\n\n* ${DESCRIPTION} [Try this challenge in Secure Code Warrior](${URL})`
         }
-      });
+    });
+});
+
+test('ruleProcessor should load test004 and add 4 entries based on the rule id, rule name and tags (x2)', async () => {
+    const sarif = await sarifLoader.load('./fixtures/test004.sarif');
+    const NAME = 'AAA';
+    const DESCRIPTION = 'bbb';
+    const URL = 'ccc';
+    const VIDEOS = ['ddd'];
+    directLinking.getTrainingData.mockResolvedValue({
+        name: NAME,
+        description: DESCRIPTION,
+        url: URL,
+        videos: VIDEOS
+    });
+    await ruleProcessor.process(sarif.runs[0]);
+
+    // expect material added to help.text and help.markdown
+    expect(sarif.runs[0].tool.driver.rules[0]).toEqual({
+        "id": "TEST01 CWE-22",
+        "name": "Test 01 rule name cwe: 23",
+        "messageStrings": {
+            "default": {
+                "text": "This is the message text. It might be very long."
+            }
+        },
+        "shortDescription": {
+            "text": "Failed to release dynamic memory."
+        },
+        "fullDescription": {
+            "text": "Unused variables, imports, functions or classes may be a symptom of a bug and should be examined carefully."
+        },
+        "help": {
+            "text": `some help text\n\nSecure Code Warrior Training:\n\n[CWE 22] ${NAME} [What is this?](${VIDEOS[0]})\n\n${DESCRIPTION} [Try this challenge in Secure Code Warrior](${URL})\n\n[CWE 23] ${NAME} [What is this?](${VIDEOS[0]})\n\n${DESCRIPTION} [Try this challenge in Secure Code Warrior](${URL})\n\n[CWE 24] ${NAME} [What is this?](${VIDEOS[0]})\n\n${DESCRIPTION} [Try this challenge in Secure Code Warrior](${URL})\n\n[CWE 25] ${NAME} [What is this?](${VIDEOS[0]})\n\n${DESCRIPTION} [Try this challenge in Secure Code Warrior](${URL})`,
+            "markdown": `markdown version some link [here](https://github.com)\n\n## Secure Code Warrior Training\n\n#### [CWE 22] ${NAME} *[What is this?](${VIDEOS[0]})*\n\n* ${DESCRIPTION} [Try this challenge in Secure Code Warrior](${URL})\n\n#### [CWE 23] ${NAME} *[What is this?](${VIDEOS[0]})*\n\n* ${DESCRIPTION} [Try this challenge in Secure Code Warrior](${URL})\n\n#### [CWE 24] ${NAME} *[What is this?](${VIDEOS[0]})*\n\n* ${DESCRIPTION} [Try this challenge in Secure Code Warrior](${URL})\n\n#### [CWE 25] ${NAME} *[What is this?](${VIDEOS[0]})*\n\n* ${DESCRIPTION} [Try this challenge in Secure Code Warrior](${URL})`
+        },
+        "properties": {
+            "tags": [
+                "Tag A",
+                "cwE-24",
+                "Tag B",
+                "Cwe_25"
+            ]
+        }
+    });
 });
